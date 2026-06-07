fa77a7873a8bfce17f2d5586521e7182618deebe
warrior <sarfaroshwarrior@gmail.com>
2026-06-07 21:51:05 +0500

    click
    on click sound play'

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ var audio = document.createElement('audio');
 audio.setAttribute('src', 'music/sound.mp3' );
 audio.loop = true;
 audio.play();
+
+document.addEventListener('click', () => {
+    audio.play().catch(error => {
+        console.log("Error", error);
+    });
+}, { once: true });
